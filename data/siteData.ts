550d38ab357cbf0b8d662f39ed41baad188a3840
Khanh Dinh <khanh.dinh@netpower.no>
2023-05-03 14:10:47 +0700

    init

---
+++ data/siteData.ts
@@ -0,0 +1,17 @@
+const siteData = {
+  title: 'Beiryu Blog',
+  author: 'Beiryu Blog',
+  headerTitle: 'Welcome To Beiryu Blog',
+  profileUrl: '/nextjs-logo.png',
+  headerDescription: 'Develop software with minimal effort',
+  footerText: '© All rights reserved',
+  language: 'en-us',
+  locale: 'en-US',
+  // socialBanner: 'https://nextjs-notion-blog-starter.vercel.app/preview/1.jpg',
+  github: 'https://github.com/beiryu',
+  twitter: 'https://github.com/beiryu',
+  // websiteUrl: 'https://nextjs-notion-blog-starter.vercel.app',
+  linkedin: 'https://www.linkedin.com/in/khanh-ndinh/'
+};
+
+export default siteData;
